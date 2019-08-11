02d28b0268bf7dafd132860269ecedcc86503ab1
Wolf Hopkins <46612966+Zevrov@users.noreply.github.com>
2019-08-11 13:03:09 -0400

    Create User.py

---
+++ models/User.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""This is the user class"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+import os
+
+
+class User(BaseModel):
+    """This is the class for user
+    Attributes:
+        email: email address
+        password: password for you login
+        first_name: first name
+        last_name: last name
+    """
+    __tablename__ = "users"
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=False)
+        last_name = Column(String(128), nullable=False)
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
