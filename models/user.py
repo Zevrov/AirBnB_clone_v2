6c5972ea105bf99202107bb8e866a94740123670
Wolf Hopkins <719@holbertonschool.com>
2019-08-12 14:59:50 -0400

    added can be nullable to relevant fields

--- models/user.py
+++ models/user.py
@@ -12,8 +12,8 @@ class User(BaseModel, Base):
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
         places = relationship("Place", back_populates="user")
         reviews = relationship("Review", back_populates="user")
     else:
