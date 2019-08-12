96cba37fcc2d8df1ebff37e9a4e83bbb595eb5a7
Wolf Hopkins <719@holbertonschool.com>
2019-08-12 15:18:38 -0400

    fixing pep8

--- models/place.py
+++ models/place.py
@@ -58,30 +58,30 @@ class Place(BaseModel, Base):
         amenity_ids = []
 
         @property
-	    def reviews(self):
-	        """Review Getter"""
-	        r_dic = models.storage.all('Review')
-	        r_list = []
-	        for index in reviews_dict.values():
-	            if index.place_id == self.id:
-	                r_list.append(index)
+        def reviews(self):
+        """Review Getter"""
+        r_dic = models.storage.all('Review')
+        r_list = []
+        for index in reviews_dict.values():
+            if index.place_id == self.id:
+                r_list.append(index)
 
-	            return i
+            return i
 
-	    @property
-	    def amenities(self):
-	        """Getter for amenities"""
-	        object_list = []
-	        objs = models.storage.all('Amenity')
-	        for index in objs.values():
-	            if index.id in amenity_id:
-	                object_list.append(index)
+        @property
+        def amenities(self):
+            """Getter for amenities"""
+            object_list = []
+           objs = models.storage.all('Amenity')
+            for index in objs.values():
+                if index.id in amenity_id:
+                    object_list.append(index)
 
-	            return object_list
+                return object_list
 
-	    @amenities.setter
-	    def amenities(self, obj):
-	        """ameneties setter"""
-	        if isinstance(obj, Amenity):
-	            if self.id == obj.place_id:
-	                self.amenity_ids.append(obj.id)
+        @amenities.setter
+        def amenities(self, obj):
+            """ameneties setter"""
+            if isinstance(obj, Amenity):
+                if self.id == obj.place_id:
+                    self.amenity_ids.append(obj.id)
