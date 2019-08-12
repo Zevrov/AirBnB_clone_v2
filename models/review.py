0593be1d8df43c48efe8c4492bda1f6d3ede5043
Wolf Hopkins <719@holbertonschool.com>
2019-08-12 13:56:56 -0400

    added tablename for reviews

--- models/review.py
+++ models/review.py
@@ -7,6 +7,7 @@ import os
 
 class Review(BaseModel, Base):
     """This is the class for Review"""
+    __tablename__ = "reviews"
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         text = Column(String(1024), nullable=False)
         place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
