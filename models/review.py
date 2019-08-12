56f4fda76625b9981d545c9681cd3e55775c8f9d
Wolf Hopkins <719@holbertonschool.com>
2019-08-12 14:10:11 -0400

    another typo

--- models/review.py
+++ models/review.py
@@ -13,7 +13,7 @@ class Review(BaseModel, Base):
         text = Column(String(1024), nullable=False)
         place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-        place = relationship("Place", back_populates="place")
+        place = relationship("Place", back_populates="reviews")
 
     else:
         place_id = ""
