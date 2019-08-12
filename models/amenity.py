d27ae2feb9334f557da11d3e311b58b0425861bd
Wolf Hopkins <719@holbertonschool.com>
2019-08-12 13:00:15 -0400

    got rid of space

--- models/amenity.py
+++ models/amenity.py
@@ -8,7 +8,7 @@ from models.place import place_amenity
 
 class Amenity(BaseModel):
     """Amenity attributes"""
-     __tablename__ = "amenities"
+    __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
     place_amenities = relationship("Place",
                                    secondary=place_amenity)
