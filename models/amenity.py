c6ed627088e2c5908ee441d591a57cdf61f30ed4
Wolf Hopkins <719@holbertonschool.com>
2019-08-12 13:01:50 -0400

    fixed indentation errors in amenities

--- models/amenity.py
+++ models/amenity.py
@@ -14,30 +14,30 @@ class Amenity(BaseModel):
                                    secondary=place_amenity)
 
     @property
-        def reviews(self):
-            """Review Getter"""
-            r_dic = models.storage.all('Review')
-            r_list = []
-            for index in reviews_dict.values():
-                if index.place_id == self.id:
-                    r_list.append(index)
+    def reviews(self):
+        """Review Getter"""
+        r_dic = models.storage.all('Review')
+        r_list = []
+        for index in reviews_dict.values():
+            if index.place_id == self.id:
+                r_list.append(index)
 
             return i
 
     @property
-        def amenities(self):
-            """Getter for amenities"""
-            object_list = []
-            objs = models.storage.all('Amenity')
-            for index in objs.values():
-                if index.id in amenity_id:
-                    object_list.append(index)
+    def amenities(self):
+        """Getter for amenities"""
+        object_list = []
+        objs = models.storage.all('Amenity')
+        for index in objs.values():
+            if index.id in amenity_id:
+                object_list.append(index)
 
             return object_list
 
     @amenities.setter
-        def amenities(self, obj):
-            """ameneties setter"""
-            if isinstance(obj, Amenity):
-                if self.id == obj.place_id:
-                    self.amenity_ids.append(obj.id)
+    def amenities(self, obj):
+        """ameneties setter"""
+        if isinstance(obj, Amenity):
+            if self.id == obj.place_id:
+                self.amenity_ids.append(obj.id)
