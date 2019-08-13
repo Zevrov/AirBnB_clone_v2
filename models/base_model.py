d4ee874bc2d0e80189572a1df9baac770e6a1860
Wolf Hopkins <719@holbertonschool.com>
2019-08-13 12:07:39 -0400

    Update base_model.py
    remove redundant code

--- models/base_model.py
+++ models/base_model.py
@@ -23,11 +23,6 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instantiation of base model class"""
-        if (len(kwargs) == 0):
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-
         if "id" not in kwargs:
             self.id = str(uuid.uuid4())
 
