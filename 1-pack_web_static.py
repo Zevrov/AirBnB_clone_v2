58a9e7f037473d220e36555c4db1a9dfb87f94d1
Wolf Hopkins <719@holbertonschool.com>
2019-08-19 17:20:48 -0400

    none typo

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,9 +9,12 @@ import fabric.api
 
 def do_pack():
     """compress + bundle local sweb files"""
-    if isdir("versions") is False:
-            local("mkdir versions")
-    time = datetime.now().strftime("%Y%m%d%H%M%S")
-    file = "versions/web_static_{}.tgz".format(time)
-    fabric.api.local('tar -cvzf ' + file + ' web_static')
-    return file
+    try:
+        if isdir("versions") is False:
+                local("mkdir versions")
+        time = datetime.now().strftime("%Y%m%d%H%M%S")
+        file = "versions/web_static_{}.tgz".format(time)
+        fabric.api.local('tar -cvzf ' + file + ' web_static')
+        return file
+    except:
+        return None
