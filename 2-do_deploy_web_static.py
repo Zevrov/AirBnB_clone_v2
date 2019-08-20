3a6eec23155fd6792aeaa108fd5a401311f7f44b
Wolf Hopkins <719@holbertonschool.com>
2019-08-19 20:03:34 -0400

    update to the script

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """destribute an archive to a web server"""
 
-import fabric.api
+from fabric.api import *
 import os
 import os.path
 import datetime
-fabric.api.env.hosts = ['34.73.58.99', '34.74.6.218']
+env.hosts = ['34.73.58.99', '34.74.6.218']
 
 
 def do_deploy(archive_path):
@@ -13,18 +13,18 @@ def do_deploy(archive_path):
     if os.path.exists(archive_path) is False:
         return False
 
-path = archive_path.split('/')[1]
-target = '/data/web_static/releases/' + path
-
+    file_name = os.path.splitext(os.path.split(archive_path)[1])[0]
+    target = '/data/web_static/releases/' + file_name
     try:
-        fabric.api.put(archive_path, "/tmp/")
-        fabric.api.run('sudo mkdir -p ' + target + '/')
-        fabric.api.run('sudo tar -xzf /tmp/' + path + ' -C ' + target + '/')
-        fabric.api.run('sudo rm /tmp/' + path)
-        fabric.api.run('sudo mv ' + target + '/web_static/* ' + target + '/')
-        fabric.api.run('sudo rm -rf ' + target + '/web_static')
-        fabric.api.run('sudo rm -rf /data/web_static/current')
-        fabric.api.run('sudo ln -s ' + target + ' /data/web_static/current')
+        path = put(archive_path, '/tmp/' + file_name)
+        path = path[0]
+        run('sudo mkdir -p ' + target)
+        run('sudo tar -xzf /tmp/' + file_name + ' -C ' + target + '/')
+        run('sudo rm /tmp/' + path)
+        run('sudo mv ' + target + '/web_static/* ' + target + '/')
+        run('sudo rm -rf ' + target + '/web_static')
+        run('sudo rm -rf /data/web_static/current')
+        run('sudo ln -s ' + target + '/ /data/web_static/current')
         print('deploy success')
         return True
     except:
