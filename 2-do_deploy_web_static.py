155bfb96689f97acb565d84067b7dd56f861ebb1
Wolf Hopkins <719@holbertonschool.com>
2019-08-19 20:13:26 -0400

    fairly confident this works

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -15,11 +15,11 @@ def do_deploy(archive_path):
 
     file_name = os.path.splitext(os.path.split(archive_path)[1])[0]
     target = '/data/web_static/releases/' + file_name
+    path = archive_path.split('/')[1]
     try:
-        path = put(archive_path, '/tmp/' + file_name)
-        path = path[0]
+        put(archive_path, "/tmp/")
         run('sudo mkdir -p ' + target)
-        run('sudo tar -xzf /tmp/' + file_name + ' -C ' + target + '/')
+        run('sudo tar -xzf /tmp/' + path + ' -C ' + target + '/')
         run('sudo rm /tmp/' + path)
         run('sudo mv ' + target + '/web_static/* ' + target + '/')
         run('sudo rm -rf ' + target + '/web_static')
