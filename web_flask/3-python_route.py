74dcbe43a2160995cca53fbfe654672e56a60983
Wolf Hopkins <719@holbertonschool.com>
2019-09-04 11:44:44 -0400

    finished 3, testing vs checker

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""starts a flask web app"""
+
+from flask import Flask
+
+application = Flask(__name__)
+
+
+@application.route('/', strict_slashes=False)
+def hello_world():
+    """prints hello_world"""
+    return "Hello HBNB!"
+
+
+@application.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """prints hbnb"""
+    return "HBNB"
+
+
+@application.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """first function with a variable"""
+    return 'C ' + text.replace('_', ' ')
+
+
+@application.route('/python/')
+@application.route('/python/<text>')
+def python(text='is cool'):
+    """like the one on line 21, but with 'python' instead of 'c'"""
+    return 'Python ' + text.replace('_', ' ')
+
+
+if __name__ == '__main__':
+    application.run(host='0.0.0.0', port=5000)
