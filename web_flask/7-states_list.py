0d2bbedfb70d2aabf37b648bec17e084121399bf
Wolf Hopkins <719@holbertonschool.com>
2019-09-04 12:58:06 -0400

    pep8 fix line 11

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,6 +8,7 @@ from flask import render_template
 application = Flask(__name__)
 application.url_map.strict_slashes = False
 
+
 @application.teardown_appcontext
 def closeStorageAfterRequest(error):
     """close and reload the storage between requests"""
